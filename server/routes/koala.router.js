789efbd4e917a8039fe4d206736355d039bd9692
Zakariye Abdi <zak.abdi94@gmail.com>
2023-02-02 14:42:15 -0600

    finished the GET

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,31 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pg = require("pg");
+// 'pool' represents a network connection to the DB
+// we call 'pool.query()' inside our router
+const pool = new pg.Pool({
+    database: "koala",
+    host: "localhost",
+    port: 5432,
+});
 
 // GET
-
+koalaRouter.get('/',(res,req)=> {
+    let sqlQuery = `
+    SELECT * FROM koala
+    ORDER BY "name" ASC
+    `; 
+    pool.query(sqlQuery)
+    .then ((dbRes)=> {
+        console.log('Database Works!!!');
+        res.send(dbRes.rows)
+    }).catch((err) => {
+        console.log ('Error Database failed',err);
+        res.sendStatus(500);
+    })
+    
+})
 
 // POST
 
