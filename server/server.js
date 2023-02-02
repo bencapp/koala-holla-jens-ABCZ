3ae47063fe28585e785694b1095fc27e511cc531
Ben Capp <bentucker333@gmail.com>
2023-02-02 14:00:36 -0600

    added pg to server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const pg = require("pg");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
-const koalaRouter = require('./routes/koala.router')
+const koalaRouter = require("./routes/koala.router");
+// 'pool' represents a network connection to the DB
+// we call 'pool.query()' inside our router
+const pool = new pg.Pool({
+  database: "koala",
+  host: "localhost",
+  port: 5432,
+});
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("server/public"));
 
 // ROUTES
-app.use('/koalas', koalaRouter)
+app.use("/koalas", koalaRouter);
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+  console.log("listening on port", PORT);
 });
